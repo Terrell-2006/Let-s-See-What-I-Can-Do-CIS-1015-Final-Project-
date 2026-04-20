a3d3225081fccae0dd3ae5ee4d128487e175b43c
Terrell-2006 <tuu63941@temple.edu>
2026-04-19 23:19:23 -0400

    Update Main Code .py

--- Main Code .py
+++ Main Code .py
@@ -1,3 +1,25 @@
+# DNA Analyzer & Mutation Classifier
+
+# Full RNA Codon Table
+codon_table = {
+    'UUU':'F','UUC':'F','UUA':'L','UUG':'L',
+    'CUU':'L','CUC':'L','CUA':'L','CUG':'L',
+    'AUU':'I','AUC':'I','AUA':'I','AUG':'M',
+    'GUU':'V','GUC':'V','GUA':'V','GUG':'V',
+    'UCU':'S','UCC':'S','UCA':'S','UCG':'S',
+    'CCU':'P','CCC':'P','CCA':'P','CCG':'P',
+    'ACU':'T','ACC':'T','ACA':'T','ACG':'T',
+    'GCU':'A','GCC':'A','GCA':'A','GCG':'A',
+    'UAU':'Y','UAC':'Y','UAA':'STOP','UAG':'STOP',
+    'CAU':'H','CAC':'H','CAA':'Q','CAG':'Q',
+    'AAU':'N','AAC':'N','AAA':'K','AAG':'K',
+    'GAU':'D','GAC':'D','GAA':'E','GAG':'E',
+    'UGU':'C','UGC':'C','UGA':'STOP','UGG':'W',
+    'CGU':'R','CGC':'R','CGA':'R','CGG':'R',
+    'AGU':'S','AGC':'S','AGA':'R','AGG':'R',
+    'GGU':'G','GGC':'G','GGA':'G','GGG':'G'
+    }
+
 # Validate DNA 
 def validate_dna(seq):
     return all(base in "ATCG" for base in seq)
