adb5d779dc2b98d98cb2bb8db0f9b37419c74edd
Terrell-2006 <tuu63941@temple.edu>
2026-04-19 22:51:12 -0400

    Update Main Code .py

--- Main Code .py
+++ Main Code .py
@@ -5,3 +5,18 @@ def validate_dna(seq):
 # Transcription 
 def transcribe(seq):
     return seq.replace("T", "U")
+
+# --- Translation ---
+def translate(rna):
+    protein = ""
+    for i in range(0, len(rna)-2, 3):
+        codon = rna[i:i+3]
+        amino = codon_table.get(codon, "?")
+        if amino == "STOP":
+            break
+        protein += amino
+    return protein
+
+# --- GC Content ---
+def gc_content(seq):
+    return round((seq.count("G") + seq.count("C")) / len(seq) * 100, 2)
